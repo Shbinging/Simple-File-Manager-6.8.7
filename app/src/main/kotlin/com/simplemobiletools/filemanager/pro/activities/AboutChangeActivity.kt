16d0d7128fa40afa8b9d011c15a83bb9744f63f9
Shbinging <1436775971@qq.com>
2021-11-20 02:34:52 +0800

    增加一个界面

--- app/src/main/kotlin/com/simplemobiletools/filemanager/pro/activities/AboutChangeActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/pro/activities/AboutChangeActivity.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.simplemobiletools.filemanager.pro.R
 import android.view.View
+import kotlinx.android.synthetic.main.activity_about_change.*
 
 class AboutChangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_change)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+    fun clickBack(view: View) {
+        finish()
     }
 }
